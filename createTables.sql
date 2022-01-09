06eac4be612aca150b72b09369855c7932d2a500
Piyush Gupta <guptapiyush963@gmail.com>
2022-01-09 22:04:04 +0530

    Minor Changes in ORDERS Query.

--- createTables.sql
+++ createTables.sql
@@ -3,4 +3,4 @@ CREATE TABLE CUSTOMER (id varchar(50) PRIMARY KEY, name varchar(25), email_id va
 DROP TABLE BOOK;
 CREATE TABLE BOOK (id varchar(50) PRIMARY KEY, name varchar(20),author varchar(20),quantity int, price number);
 DROP TABLE ORDERS;
-CREATE TABLE ORDERS(id varchar(50) PRIMARY KEY, customer_id varchar(50), order_date Date, quantity int, price number, FOREIGN KEY (customer_id) REFERENCES CUSTOMER(id));
+CREATE TABLE ORDERS(id varchar(50) PRIMARY KEY, customer_id varchar(50), order_date Date, book_count int, price number, FOREIGN KEY (customer_id) REFERENCES CUSTOMER(id));
