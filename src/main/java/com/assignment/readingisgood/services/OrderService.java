81b863dc03337a03e5090e66f42cd89edbc0f3d4
Piyush Gupta <guptapiyush963@gmail.com>
2022-01-10 04:07:43 +0530

    Removed unused code.

--- src/main/java/com/assignment/readingisgood/services/OrderService.java
+++ src/main/java/com/assignment/readingisgood/services/OrderService.java
@@ -1,9 +1,6 @@
 package com.assignment.readingisgood.services;
 
-import com.assignment.readingisgood.exceptions.BookNotFound;
-import com.assignment.readingisgood.exceptions.InvalidQuantity;
-import com.assignment.readingisgood.exceptions.OrderNotFound;
-import com.assignment.readingisgood.exceptions.OutOfStockException;
+import com.assignment.readingisgood.exceptions.*;
 import com.assignment.readingisgood.models.Order;
 import com.assignment.readingisgood.models.OrderRequest;
 
@@ -13,7 +10,7 @@ import java.util.List;
 
 public interface OrderService {
     @Transactional
-    String bookOrder(OrderRequest orderRequest) throws InvalidQuantity, BookNotFound, OutOfStockException;
+    String bookOrder(OrderRequest orderRequest) throws BookNotFound, OutOfStockException, UserNotExist, InvalidInput;
     Order getOrderById(String order_id) throws OrderNotFound;
     List<Order> getOrderByDate(Date startDate, Date endDate);
     List<Order> getCustomerOrders(String customerId);
