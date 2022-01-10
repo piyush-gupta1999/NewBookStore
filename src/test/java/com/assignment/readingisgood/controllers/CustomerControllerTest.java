a65c311431d9a63afe06e2d71bb2dad437071546
Piyush Gupta <guptapiyush963@gmail.com>
2022-01-10 17:35:16 +0530

    Test cases added for customer controller.

--- src/test/java/com/assignment/readingisgood/controllers/CustomerControllerTest.java
+++ src/test/java/com/assignment/readingisgood/controllers/CustomerControllerTest.java
@@ -1,9 +1,13 @@
 package com.assignment.readingisgood.controllers;
 
+import com.assignment.readingisgood.exceptions.BookNotFound;
+import com.assignment.readingisgood.exceptions.OrderNotFound;
 import com.assignment.readingisgood.models.Customer;
 import com.assignment.readingisgood.models.CustomerResponse;
+import com.assignment.readingisgood.models.Order;
 import com.assignment.readingisgood.models.Response;
 import com.assignment.readingisgood.repository.CustomerRepository;
+import com.assignment.readingisgood.repository.OrderRepository;
 import com.assignment.readingisgood.services.CustomerServices;
 import com.assignment.readingisgood.services.OrderService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,16 +20,17 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.reactive.server.StatusAssertions;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
-import static org.springframework.http.RequestEntity.post;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = CustomerController.class)
@@ -44,15 +49,23 @@ public class CustomerControllerTest {
     @MockBean
     private CustomerRepository customerRepository;
 
-    String id = UUID.randomUUID().toString();
-    Response mockCustomerAPIResponse = new CustomerResponse("Success",id);
-    Customer mockCustomer = new Customer("Piyush Gupta", "guptapiyush963@gmail.com","7015775512");
+    @MockBean
+    private OrderRepository orderRepository;
+
+    String id;
+    Response mockCustomerAPIResponse;
+    Customer mockCustomer;
+    Date date;
+    Exception exception;
 
     @Before
     public void setUp() {
-        Customer mockCustomer = new Customer("Piyush Gupta", "guptapiyush963@gmail.com","7015775512");
-        mockCustomer.setId(id);
-        Mockito.when(customerRepository.existsById(mockCustomer.getId()))
+         id = UUID.randomUUID().toString();
+         mockCustomerAPIResponse = new CustomerResponse("Success",id);
+         mockCustomer = new Customer("Piyush Gupta", "guptapiyush963@gmail.com","7015775512");
+         date = new Date();
+         mockCustomer.setId(id);
+         Mockito.when(customerRepository.existsById(mockCustomer.getId()))
                 .thenReturn(true);
     }
 
@@ -65,7 +78,30 @@ public class CustomerControllerTest {
                 .content(asJsonString(mockCustomer)))
                 .andExpect(status().isForbidden());
     }
+    @Test
+    public void getCustomerOrders_success() throws Exception{
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("1",id,date,100,1000.00));
+        Mockito.when(orderService.getCustomerOrders(id)).thenReturn(orders);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/customers/"+id)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",notNullValue()))
+                .andExpect(jsonPath("$.status",is("Success")));
+    }
 
+    @Test
+    public void getCustomerOrdersEmpty() throws Exception{
+        exception = new OrderNotFound("Order Not Found with Id:2");
+        Mockito.when(orderService.getCustomerOrders(id)).thenReturn(null);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/customers/"+id)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is("Success")))
+                .andExpect(jsonPath("$.description", is(new ArrayList())));
+    }
     public static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
